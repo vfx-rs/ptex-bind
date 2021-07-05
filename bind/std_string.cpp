448b726f2d07b614089c2e4cb4df4e698d376bb8
David Aguilar <davvid@gmail.com>
2021-07-05 12:03:59 -0700

    bind: update std_string.cpp for _GLIBCXX_USE_CXX11_ABI
    Copy std_string.cpp's bindings for std::string from cppmm/test/std/bind.

--- bind/std_string.cpp
+++ bind/std_string.cpp
@@ -6,20 +6,29 @@ namespace cppmm_bind {
 
 namespace std {
 
+#if defined(_GLIBCXX_USE_CXX11_ABI)
+#if _GLIBCXX_USE_CXX11_ABI
+namespace std = ::std::__cxx11;
+#else
+namespace std = ::std;
+#endif
+#elif defined(_LIBCPP_VERSION)
+namespace std = ::std::_LIBCPP_ABI_NAMESPACE;
+#else
+namespace std = ::std;
+#endif
+
 class basic_string {
 public:
-    using BoundType = ::std::string;
+    using BoundType = std::basic_string<char>;
 
-    basic_string();
-    basic_string(const ::std::string& rhs);
-
+    basic_string(const char* s, ::std::string::size_type count);
     ::std::string& assign(const char* s, ::std::string::size_type count);
     const char* c_str() const;
 
 } CPPMM_OPAQUEBYTES CPPMM_IGNORE_UNBOUND;
 
 using string = ::std::string;
-
 } // namespace std
 
 } // namespace cppmm_bind
