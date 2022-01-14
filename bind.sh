6bcbac452422992873fa7019b5d92b2409d3b4a2
David Aguilar <davvid@gmail.com>
2022-01-14 14:03:44 -0800

    bind.sh: rearrange directory structure
    Move ptex-sys/ up into the top-level. It is a build artifact
    but it is convenient to have it here.
    
    The .gitignore pattern is to allow us to have ptex/
    ptex-sys/ and ptex-rs/ adjacent to themselves at the
    top-level. The ptex/ location is not required, as we
    look at ${PTEX_ROOT}, but the .gitignore allows it in-tree.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- bind.sh
+++ bind.sh
@@ -7,7 +7,7 @@ then
   exit 1
 fi
 
-CMAKE_PREFIX_PATH=${PTEX_ROOT}
+CMAKE_PREFIX_PATH=${PTEX_ROOT}${CMAKE_PREFIX_PATH:+:$CMAKE_PREFIX_PATH}
 export CMAKE_PREFIX_PATH
 
 PTEX_MAJOR_VERSION=2
@@ -17,35 +17,32 @@ PTEX_PATCH_VERSION=1
 num_procs=$(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || echo 4)
 
 rm -rf build
+mkdir -p build/ast
 
 astgen bind -u -v 1 -o build/ast -- "$@" -I${PTEX_ROOT}/include
 
-asttoc build/ast -o build -p ptex -L $PTEX_ROOT/lib -l Ptex \
-        --fp Ptex --tll Ptex::Ptex_dynamic \
-        -major ${PTEX_MAJOR_VERSION} \
-        -minor ${PTEX_MINOR_VERSION} \
-        -patch ${PTEX_PATCH_VERSION}
+asttoc build/ast -o . -p ptex \
+    --fp Ptex --tll Ptex::Ptex_dynamic \
+    -L ${PTEX_ROOT}/lib64 -L ${PTEX_ROOT}/lib -l Ptex \
+    -major ${PTEX_MAJOR_VERSION} \
+    -minor ${PTEX_MINOR_VERSION} \
+    -patch ${PTEX_PATCH_VERSION}
 
-mkdir -p ptex-sys/build
-(
-  cd ./ptex-sys/build
-  cmake -D CMAKE_INSTALL_PREFIX=../dist ../ptex-c
-  cmake --build . --target all --parallel ${num_procs}
-)
+cmake -B ptex-sys/build -D CMAKE_INSTALL_PREFIX="${PWD}/dist" ptex-sys/ptex-c
+cmake --build ptex-sys/build --target all --parallel ${num_procs}
+cmake --build ptex-sys/build --target install --parallel ${num_procs}
 
-mkdir -p ptex-sys/target/debug/deps
-mkdir -p ptex-sys/target/release/deps
-
-cp "${PTEX_ROOT}/lib"/libPtex.* ptex-sys/target/debug/deps
-cp "${PTEX_ROOT}/lib"/libPtex.* ptex-sys/target/release/deps
-
-cp test.rs ptex-sys/src/
+for deps in ptex-{rs,sys}/target/{debug,release}/deps
+do
+    mkdir -p "$deps"
+    cp "${PTEX_ROOT}"/lib*/libPtex.* "$deps"
+done
 
 echo
 echo '#' Run these commands following to build and test the rust bindings:
 echo
 echo ' ' export CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
 echo ' ' cargo build --manifest-path=ptex-sys/Cargo.toml
+echo ' ' cargo build --manifest-path=ptex-rs/Cargo.toml
+echo ' ' cargo test --manifest-path=ptex-rs/Cargo.toml
 echo ' '
-echo ' ' cp test.rs ptex-sys/src/
-echo ' ' cargo test --manifest-path=ptex-sys/Cargo.toml
